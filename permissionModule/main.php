889225a0acf24935441318ae88ba383163e0704e
selva <selva@oddcounts.com>
2023-01-03 15:49:47 +0530

    main file added

---
+++ permissionModule/main.php
@@ -0,0 +1,16 @@
+<!-- all-access-funcitonality code -->
+
+<?php
+
+class main {
+
+    public function checkuser()
+    {
+        return "Check the user funcitonality";
+    }
+
+    public function adduser()
+    {
+        return "add the new user functionality";
+    }
+}
