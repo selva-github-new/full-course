d6bd50a48fff6afe2a685b443278abb7f8c42280
selva <selva@oddcounts.com>
2023-01-03 15:36:02 +0530

    permission branch
    permission branch developer code functionality

---
+++ permission/Readme.md
@@ -0,0 +1,13 @@
+<!-- this is permission module code functionality -->
+
+<!-- useing the moule only for super admins -->
+
+# Role ##
+
+# editor : #45582255
+
+# admin : true
+
+# superAdmin : false
+
+# Author : true
---
+++ permission/permission.php
@@ -0,0 +1,15 @@
+<?php
+
+class permission {
+    public function add(){
+        return 'add permission funcitonality';
+    }
+
+    public function edit(){
+        return 'add permission funcitonality';
+    }
+
+    public function create(){
+        return 'add permission funcitonality';
+    }
+}
