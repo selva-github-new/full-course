bf8d6e8d9b120bb6c365b8a200859fd5d370de2e
selva <selva@oddcounts.com>
2023-01-03 16:31:04 +0530

    gallery module functionality

---
+++ Gallery/main.php
@@ -0,0 +1,16 @@
+<!-- gallery main functionality -->
+
+<?php
+
+class main {
+
+    // all file functionality
+
+    public function FunctionName($name)
+    {
+        # code...
+
+        return "gallery functional functionality";
+    }
+
+}
